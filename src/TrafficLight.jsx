61d88e9872149e6b6c6e36845fd8a8e979cc669b
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-06 11:00:41 +0530

    additional changes

--- src/TrafficLight.jsx
+++ src/TrafficLight.jsx
@@ -1,41 +1,55 @@
 import { useEffect, useState } from "react"
 import "./style.css"
 
+const timeObject = {
+    red: 5000,
+    yellow: 10000,
+    green: 7000
+}
+
 
 const TrafficLight = () => {
 
     const [active, setActive] = useState("red")
-   
+
+    const [time, setTime] = useState(timeObject);
+
 
     useEffect(() => {
         if (active === "red") {
             setTimeout(() => {
                 setActive("green")
-            }, [5000])
+            }, [time.red])
         }
         else if (active === 'yellow') {
             setTimeout(() => {
                 setActive("red")
-            }, [10000])
+            }, [time.yellow])
         } else {
             setTimeout(() => {
                 setActive("yellow")
-            }, [7000])
+            }, [time.green])
         }
     }, [active])
 
     return (
-        <div className="container">
-            <div
-                style={{ opacity: active === 'red' ? 1 : 0.5 }}
-                className="color red">R</div>
-            <div
-                style={{ opacity: active === 'green' ? 1 : 0.5 }}
-                className="color green">G</div>
-            <div
-                style={{ opacity: active === 'yellow' ? 1 : 0.5 }}
-                className="color yellow">Y</div>
+        <div className="content">
+            <div className="container">
+                <div
+                    style={{ opacity: active === 'red' ? 1 : 0.5 }}
+                    className="color red">R</div>
+                <div
+                    style={{ opacity: active === 'green' ? 1 : 0.5 }}
+                    className="color green">G</div>
+                <div
+                    style={{ opacity: active === 'yellow' ? 1 : 0.5 }}
+                    className="color yellow">Y</div>
+            </div>
+            <div className="details">
+                <p>{active.toUpperCase()} LIGHT</p>
+            </div>
         </div>
+     
     )
 }
 
