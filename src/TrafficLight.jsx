8924baa87377bbab61cbea036a19e7aea99c4bfe
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-06 10:46:39 +0530

    logic for red light

--- src/TrafficLight.jsx
+++ src/TrafficLight.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react"
 import "./style.css"
 
 
 const TrafficLight = () => {
+
+    const [active, setActive] = useState("red")
+
+    useEffect(() => {
+        if (active === "red") {
+            setTimeout(() => {
+                setActive("yellow")
+            }, [5000])
+        } 
+    }, [])
+
     return (
         <div className="container">
-            <div className="color red">R</div>
-            <div className="color green">G</div>
-            <div className="color yellow">Y</div>
+            <div
+                style={{ opacity: active === 'red' ? 1 : 0.5 }}
+                className="color red">R</div>
+            <div
+                style={{ opacity: active === 'green' ? 1 : 0.5 }}
+                className="color green">G</div>
+            <div
+                style={{ opacity: active === 'yellow' ? 1 : 0.5 }}
+                className="color yellow">Y</div>
         </div>
     )
 }
